fcd6de76b7a24fe913b92aef369ad00bd635a272
s777610 <s777610@gmail.com>
2018-11-24 22:29:24 -0800

    add upload images features

--- Upload_images/resources/confirmation.py
+++ Upload_images/resources/confirmation.py
@@ -1,28 +1,28 @@
-from time import time
 from flask import render_template, make_response
 from flask_restful import Resource
 import traceback
+from time import time
 
 from models.confirmation import ConfirmationModel
 from schemas.confirmation import ConfirmationSchema
 from models.user import UserModel
-
 from libs.mailgun import MailGunException
-from libs.strings import gettext # every resource import this, refresh() run once
-
-
+from libs.strings import gettext
 
 confirmation_schema = ConfirmationSchema()
 
+
 class Confirmation(Resource):
+    # returns the confirmation page
     @classmethod
     def get(cls, confirmation_id: str):
-        """Return confirmation HTML page after user confirmating."""
         confirmation = ConfirmationModel.find_by_id(confirmation_id)
         if not confirmation:
             return {"message": gettext("confirmation_not_found")}, 404
+
         if confirmation.expired:
             return {"message": gettext("confirmation_link_expired")}, 400
+
         if confirmation.confirmed:
             return {"message": gettext("confirmation_already_confirmed")}, 400
 
@@ -36,12 +36,13 @@ class Confirmation(Resource):
             headers,
         )
 
-    
 
 class ConfirmationByUser(Resource):
     @classmethod
-    def get(self, user_id: int):
-        """Return confirmation for a given user, for testing"""
+    def get(cls, user_id: int):
+        """
+        This endpoint is used for testing and viewing Confirmation models and should not be exposed to public.
+        """
         user = UserModel.find_by_id(user_id)
         if not user:
             return {"message": gettext("user_not_found")}, 404
@@ -65,14 +66,14 @@ class ConfirmationByUser(Resource):
         """
         user = UserModel.find_by_id(user_id)
         if not user:
-            return {"message": USER_NOT_FOUND}, 404
+            return {"message": gettext("user_not_found")}, 404
 
         try:
             # find the most current confirmation for the user
             confirmation = user.most_recent_confirmation  # using property decorator
             if confirmation:
                 if confirmation.confirmed:
-                    return {"message": ALREADY_CONFIRMED}, 400
+                    return {"message": gettext("confirmation_already_confirmed")}, 400
                 confirmation.force_to_expire()
 
             new_confirmation = ConfirmationModel(user_id)  # create a new confirmation
@@ -86,5 +87,3 @@ class ConfirmationByUser(Resource):
         except:
             traceback.print_exc()
             return {"message": gettext("confirmation_resend_fail")}, 500
-
-    
