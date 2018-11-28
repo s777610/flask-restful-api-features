07033f6949576aef1909df37cb8c2f3620828120
s777610 <s777610@gmail.com>
2018-11-27 18:41:05 -0800

    add OAuth features

--- 4.OAuth/app.py
+++ 4.OAuth/app.py
@@ -1,9 +1,6 @@
-import os
-
 from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
-from flask_migrate import Migrate
 from marshmallow import ValidationError
 from dotenv import load_dotenv
 
@@ -14,16 +11,11 @@ from resources.user import UserRegister, UserLogin, User
 
 app = Flask(__name__)
 load_dotenv(".env")
-app.config["DEBUG"] = True
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    "DATABASE_URI", "sqlite:///data.db"
-)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.secret_key = "jose"
+app.config.from_object("default_config")
+app.config.from_envvar("APPLICATION_SETTINGS")
 api = Api(app)
 jwt = JWTManager(app)
-migrate = Migrate(app, db)
+
 
 @app.before_first_request
 def create_tables():
@@ -42,4 +34,4 @@ api.add_resource(UserLogin, "/login")
 if __name__ == "__main__":
     db.init_app(app)
     ma.init_app(app)
-    app.run(port=5000, debug=True)
+    app.run(port=5000)
